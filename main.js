65e54f7963be3b614bcf492fa0fd510bc86d4f13
Niem Nguyen <luuniem87@gmail.com>
2019-09-06 20:18:10 -0700

    add more animations

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let blanks = document.querySelector(".blanks");
 let cardOne = document.querySelector(".card-1");
 let cardTwo = document.querySelector(".card-2");
+let answerCard = document.querySelector(".answer-card");
 let cardAnswerText = document.querySelector(".answer-text");
 let mathAddForm = document.getElementById("math-add-form");
 let inputAnswer = document.getElementById("input-answer");
@@ -8,13 +9,18 @@ let inputClass = document.querySelector(".input-class");
 let correctAnswer = document.querySelector(".correct-answer");
 let wrongAnswer = document.querySelector(".wrong-answer");
 
+
+
 function generateNumbers() {
   let blankArray = Array.from({ length: 2 }, () =>
     Math.floor(Math.random() * 5)
   );
   mathAddForm.reset();
-  mathAddForm.style.display = "block";
+  cardOne.classList.add('flipInX');
+  cardTwo.classList.add('flipInX');
+  answerCard.classList.add('flipInX');
 
+  mathAddForm.style.display = "block";
   cardOne.innerHTML = blankArray[0];
   cardTwo.innerHTML = blankArray[1];
   cardAnswerText.innerHTML = blankArray[0] + blankArray[1];
@@ -29,13 +35,22 @@ mathAddForm.addEventListener("submit", function(e) {
   if (inputAnswer.value === cardAnswerText.innerHTML) {
     inputClass.style.display = "none";
     correctAnswer.style.display = "flex";
+    cardOne.classList.remove('flipInX');
+    cardTwo.classList.remove('flipInX');
   } else {
     inputClass.style.display = "none";
     wrongAnswer.style.display = "flex";
+    cardOne.classList.add('shake');
+    cardTwo.classList.add('shake');
   }
 });
 
 function tryAgain() {
   inputClass.style.display = "flex";
   wrongAnswer.style.display = "none";
+}
+
+function resetClass(){
+  cardOne.classList.remove('flipInX','shake');
+  cardTwo.classList.remove('flipInX','shake');
 }
